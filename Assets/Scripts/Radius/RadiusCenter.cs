f9aae20e15a6f53a4d1b3976244cf38ec8711d41
陳泰儒 <hello19881990@gmail.com>
2017-01-21 11:18:45 +0800

    no message

---
+++ Assets/Scripts/Radius/RadiusCenter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class RadiusCenter 
+{
+	private static RadiusCenter Instance = null;
+
+	public static RadiusCenter GetInstance()
+	{
+		if (Instance == null) 
+		{
+			Instance = new RadiusCenter ();
+		}
+
+		return Instance;
+	}
+
+	private RadiusCenter()
+	{
+	}
+
+	public void SetLevelByScore(int score)
+	{
+	}
+
+	public float GetRedRadius()
+	{
+		return 2.0f;
+	}
+
+	public float GetGreenRadius()
+	{
+		return 1.0f;
+	}
+}
