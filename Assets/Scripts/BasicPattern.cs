aeae9c927b33230d34a9e32af2b5bc2f7b50a74e
ted <ted@randomidea.com>
2017-01-21 09:11:03 +0800

    - Basic game loop.

---
+++ Assets/Scripts/BasicPattern.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+
+public class BasicPattern : MonoBehaviour
+{
+    [SerializeField] private float _minScale = 1f;
+    [SerializeField] private float _maxScale = 2f;
+    [SerializeField] private Transform _minCheckingCircle;
+    [SerializeField] private Transform _maxCheckingCircle;
+    [SerializeField] private float _scaleSpeed = 0.25f;
+
+    private Transform _transform;
+    private bool _scaleUp = true;
+    private bool _start = false;
+
+    private void Awake()
+    {
+        _transform = transform;
+        Reset();
+    }
+
+    private void Reset()
+    {
+        _scaleUp = true;
+        _start = false;
+        _transform.localScale = Vector3.one * _minCheckingCircle.localScale.x;
+    }
+
+    private void Update()
+    {
+        if (!_start)
+        {
+            return;
+        }
+
+        OnCheckCircle();
+
+        if (_scaleUp)
+        {
+            _transform.localScale += Vector3.one * _scaleSpeed * Time.deltaTime;
+
+            if (_transform.localScale.x >= _maxScale)
+            {
+                _scaleUp = false;
+            }
+        }
+        else
+        {
+            _transform.localScale -= Vector3.one * _scaleSpeed * Time.deltaTime;
+
+            if (_transform.localScale.x < _minScale)
+            {
+                OnLossGame();
+            }
+        }
+    }
+
+    private void OnCheckCircle()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (_transform.localScale.x >= _minCheckingCircle.localScale.x && _transform.localScale.x <= _maxCheckingCircle.localScale.x)
+            {
+                _scaleUp = true;
+                OnGetScore();
+            }
+            else
+            {
+                OnLossGame();
+            }
+        }
+    }
+
+    private void OnGetScore()
+    {
+        UIManager.Instance.OnGetScore();
+    }
+
+    private void OnLossGame()
+    {
+        _start = false;
+        UIManager.Instance.OnLossGame();
+    }
+
+    public void OnStartGame()
+    {
+        Reset();
+        _start = true;
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance
+    {
+        get { return _instance; }
+    }
+    private static UIManager _instance;
+
+    [SerializeField]
+    private BasicPattern _basicPattern;
+    [SerializeField]
+    private Button _startButton;
+    [SerializeField]
+    private Text _scoreText;
+
+    private int _score = 0;
+
+    private void Awake()
+    {
+        _instance = this;
+        _startButton.onClick.AddListener(OnStartGameButtonClicked);
+    }
+
+    private void OnStartGameButtonClicked()
+    {
+        _score = 0;
+        _basicPattern.OnStartGame();
+        _startButton.gameObject.SetActive(false);
+    }
+
+    public void OnLossGame()
+    {
+        _startButton.gameObject.SetActive(true);
+    }
+
+    public void OnGetScore()
+    {
+        _score++;
+        _scoreText.text = _score.ToString();
+    }
+}
