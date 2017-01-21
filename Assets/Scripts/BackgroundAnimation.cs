8c6271e5a40c625eeae4962491c9f14a08a22df2
ted <ted@randomidea.com>
2017-01-21 10:31:00 +0800

    - Update background animation.

---
+++ Assets/Scripts/BackgroundAnimation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackgroundAnimation : MonoBehaviour
+{
+    [SerializeField] private float _speed = 1f;
+    private Material _material;
+
+    private void Awake()
+    {
+        _material = GetComponent<Image>().material;
+    }
+
+    private void Update()
+    {
+        _material.SetTextureOffset("_MainTex", new Vector2(Time.time * _speed, 0));
+    }
+}
