23d6684ce663964c14722d5035e442af8ee0c455
陳泰儒 <hello19881990@gmail.com>
2017-01-21 12:45:46 +0800

    隨機半徑介面調整 ＆ 第一版

---
+++ Assets/Scripts/Radius/Level1.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level1 : RandomRadius {
+	public Level1()
+	{
+		RedMinScale = 2.0f;
+		RedMaxScale = 10.0f;
+		GreenMinScale = 1.0f;
+		GreenMaxScale = 1.0f;
+
+	}
+}
