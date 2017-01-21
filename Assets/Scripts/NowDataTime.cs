94136f31752ac66bf0a60149782e6633cbe0d379
Jimyo <ty5534@gmail.com>
2017-01-21 13:48:17 +0800

    -讀取裝置時間完成

---
+++ Assets/Scripts/NowDataTime.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NowDataTime : MonoBehaviour {
+
+	DateTime Now;
+	void Start () 
+	{
+
+	}
+	
+	void Update () 
+	{
+		Now = DateTime.Now;
+
+		Debug.Log (Now.Hour+":"+Now.Minute+":"+Now.Second);
+		
+	}
+}
