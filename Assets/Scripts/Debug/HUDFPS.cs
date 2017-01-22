13ac2cd870b6ca326ab11de9c385c96817cf4aaa
ted <ted@randomidea.com>
2017-01-22 11:42:15 +0800

    - Remove unused Font. - Improve FPS.

--- Assets/Scripts/Debug/HUDFPS.cs
+++ Assets/Scripts/Debug/HUDFPS.cs
@@ -1,23 +1,11 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 [AddComponentMenu( "Utilities/HUDFPS")]
 public class HUDFPS : MonoBehaviour
 {
-    // Attach this to any object to make a frames/second indicator.
-    //
-    // It calculates frames/second over each updateInterval,
-    // so the display does not keep changing wildly.
-    //
-    // It is also fairly accurate at very low FPS counts (<10).
-    // We do this not by simply counting frames per interval, but
-    // by accumulating FPS for each frame. This way we end up with
-    // corstartRect overall FPS even if the interval renders something like
-    // 5.5 frames.
-
-    public Rect startRect = new Rect( 10, 10, 75, 50 ); // The rect the window is initially displayed at.
     public bool updateColor = true; // Do you want the color to change if the FPS gets low
-    public bool allowDrag = true; // Do you want to allow the dragging of the FPS window
     public  float frequency = 0.5F; // The update frequency of the fps
     public int nbDecimal = 1; // How many decimal do you want to display
 
@@ -25,10 +13,12 @@ public class HUDFPS : MonoBehaviour
     private int   frames  = 0; // Frames drawn over the interval
     private Color color = Color.white; // The color of the GUI, depending of the FPS ( R < 10, Y < 30, G >= 30 )
     private string sFPS = ""; // The fps formatted into a string.
-    private GUIStyle style; // The style the text will be displayed at, based en defaultSkin.label.
+
+    private Text text;
 
     void Start()
     {
+        text = GetComponent<Text>();
         StartCoroutine( FPS() );
     }
 
@@ -53,26 +43,11 @@ public class HUDFPS : MonoBehaviour
             accum = 0.0F;
             frames = 0;
 
+            GUI.color = updateColor ? color : Color.white;
+            text.color = updateColor ? color : Color.white;
+            text.text = sFPS;
+
             yield return new WaitForSeconds( frequency );
         }
-    }
-
-    void OnGUI()
-    {
-        // Copy the default label skin, change the color and the alignement
-        if( style == null ){
-            style = new GUIStyle( GUI.skin.label );
-            style.normal.textColor = Color.white;
-            style.alignment = TextAnchor.MiddleCenter;
-        }
-
-        GUI.color = updateColor ? color : Color.white;
-        startRect = GUI.Window(0, startRect, DoMyWindow, "");
-    }
-
-    void DoMyWindow(int windowID)
-    {
-        GUI.Label( new Rect(0, 0, startRect.width, startRect.height), sFPS + " FPS", style );
-        if( allowDrag ) GUI.DragWindow(new Rect(0, 0, Screen.width, Screen.height));
     }
 }
