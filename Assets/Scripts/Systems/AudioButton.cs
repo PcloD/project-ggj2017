67af0bf9f690dca8dbcf768be7e211116a313bd5
陳泰儒 <hello19881990@gmail.com>
2017-01-22 12:16:47 +0800

    - Added the audio button.

---
+++ Assets/Scripts/Systems/AudioButton.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioButton : MonoBehaviour 
+{
+	
+	[SerializeField]
+	private GameObject _AudioOn;
+	[SerializeField]
+	private GameObject _AudioOff;
+
+	private static readonly string AUDIO_ON = "AUDIO_ON";
+	public void Start()
+	{
+		if (PlayerPrefs.HasKey (AUDIO_ON)) 
+		{
+			PlayerPrefs.SetInt (AUDIO_ON, 1);
+		}
+
+		SetImage (IsAudioOn());
+	}
+
+	private bool IsAudioOn()
+	{
+		return (PlayerPrefs.GetInt (AUDIO_ON) == 1);
+	}
+
+	private void SetAudioOn( bool IsOn )
+	{
+		PlayerPrefs.SetInt (AUDIO_ON, IsOn?1:0);
+	}
+
+	private void SetImage( bool IsOn )
+	{
+		_AudioOn.SetActive (IsOn);
+		_AudioOff.SetActive (!IsOn);	
+	}
+
+	public void OnAudioClicked()
+	{
+		bool audioOn = IsAudioOn ();
+		SetImage (audioOn);
+		SetAudioOn (!audioOn);
+	}
+}
